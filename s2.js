75b0ee22a9e32df71d928b4a2df2cf561ea04720
k1a2v3i4 <85231549+k1a2v3i4@users.noreply.github.com>
2026-03-23 18:24:19 +0530

    Add files via upload

---
+++ s2.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.get("/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+  ]);
+});
+app.listen(3001, () => console.log("Server 2 running on port 3001"));
